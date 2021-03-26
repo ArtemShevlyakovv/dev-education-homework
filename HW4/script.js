a86d79db245c30cea217a7795ff22bcef79c504e
ArtemShevlyakovv <44527875+ArtemShevlyakovv@users.noreply.github.com>
2021-03-26 15:28:30 +0200

    Create script.js

---
+++ HW4/script.js
@@ -0,0 +1,18 @@
+
+//инпуты диапазона
+var minValueInput = document.querySelector('.minValue');
+var maxValueInput = document.querySelector('.maxValue');
+
+//кнопки
+var randomButton = document.querySelector('.random');
+var resetButton = document.querySelector('.reset');
+
+//'Невидимые' элементы
+var generNum = document.querySelector('.generatedNumber');
+
+
+
+
+
+
+
